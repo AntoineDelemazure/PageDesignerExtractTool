1af887078988d4f7a93027bc182b43127c469566
Antoine Delemazure <antoine.delemazure@madagence.com>
2023-11-17 18:23:50 +0100

    Nouvelle fonction : renommage des ID

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ Pour isoler en un instant un page Page Designer depuis un fichier library issu d
 1. Cloner le repo
 2. ouvrir une console dans le dossier du repo
 2. npm i
-3. node  index.js adresse_de_l_export_a_parcourir ID_de_la_page_a_isoler
+3. node index.js adresse_de_l_export_a_parcourir ID_de_la_page_a_isoler
+
+## Renommage des IDs
+Il faut croire que la boite noire n'est pas particulièrement douée pour faire des UUID vraiment unique, et il arrive que la page s'importe très mal à cause lien multiple vers un composant.
+Aussi existe t-il une fonction pour renommer chaque ID afin de s'assurer que cette situation ne se présente pas. Ajoutez simplement le troisième argument 'y' pour l'activer
+Ex : node index.js adresse_de_l_export_a_parcourir ID_de_la_page_a_isoler y
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const xml2js = require('xml2js');
+const uuid = require('uuid')
 
 var parser = new xml2js.Parser();
 
@@ -17,13 +18,23 @@ class ParsedLibrary {
         return null;
     }
 
-    extract(rootContent, collection) {
+    extract(rootContent, collection, newUUID) {
+        let oldUUID = rootContent['$']['content-id']
+        let replacingUUID = null;
+        if (newUUID && typeof newUUID === 'string') { // renaming is involved AND we're not at the root
+            rootContent['$']['content-id'] = newUUID;
+            console.log(`Je m'appelais ${oldUUID}, je suis désormais ${rootContent['$']['content-id']}`); // clog
+        }
         if (rootContent['content-links']) {
             collection.push(rootContent)
             let contentCollection = rootContent['content-links']
             for (let index in contentCollection[0]['content-link']) {
                 let childContent = this.findContent(contentCollection[0]['content-link'][index]['$']['content-id'])
-                this.extract(childContent, collection)
+                if (newUUID) {
+                    replacingUUID = uuid.v4()
+                    contentCollection[0]['content-link'][index]['$']['content-id'] = replacingUUID;
+                }
+                this.extract(childContent, collection, replacingUUID)
             }
         } else {
             collection.push(rootContent)
@@ -32,12 +43,12 @@ class ParsedLibrary {
     }
 }
 
-function initExtract(someKindOfObject, contentID) {
+function initExtract(someKindOfObject, contentID, renameContents) {
     let pl = new ParsedLibrary(someKindOfObject)
     let root = pl.findContent(contentID)
     let contentCollection = []
     if (root) {
-        pl.extract(root, contentCollection)
+        pl.extract(root, contentCollection, renameContents)
     }
     writeFinalFile(contentCollection)
 }
@@ -69,7 +80,7 @@ var cmdLineArgs = process.argv.slice(2)
 if (cmdLineArgs.length >= 2) {
     fs.readFile(cmdLineArgs[0], (err, data) => {
         parser.parseString(data, (err, result) => {
-            initExtract(result, cmdLineArgs[1])
+            initExtract(result, cmdLineArgs[1], cmdLineArgs.length > 2 && cmdLineArgs[2] === 'y')
         })
     })
 } else {
